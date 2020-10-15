1205bcae68400f8b1578db37ac96437f475d3817
Henrik Bulskov <bulskov@ruc.dk>
2020-10-15 11:21:24 +0200

    Create first part of the client

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class ClientProgram
+    {
+        static void Main(string[] args)
+        {
+            using var client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 5000);
+
+        }
+    }
+}
